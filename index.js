ca7c6824c3e6c548336b136b91b8056c5dc80696
Ryan <ryanmichaelgarber@gmail.com>
2025-11-13 12:35:45 -0500

    Store more useful course data Cleanly update page after refresh Better separate backend/frontend logic Improve symbol names Remove obsolete dark-mode code Make all file changes reload script

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const commander = require('commander');
 const express = require('express');
 const exec = require('child_process').exec;
+const fs = require('fs');
+const util = require('util');
 
 const program = new commander.Command();
 
@@ -19,8 +21,18 @@ program
             catch (e) {
                 console.error('Failed to open Firefox automatically. Open this URL to continue:');
                 console.error(`http://localhost:${port}/index.user.js`);
-				console.log('\n', 'More information:', e);
             }
+            let timeout = null;
+            fs.watch(__dirname, { recursive: true }, (event, file) => {
+                if (['.vscode', 'node_modules', 'package-lock.json', 'index.user.js'].includes(file)) return;
+                clearTimeout(timeout);
+                timeout = setTimeout(async () => {
+                    let js = await util.promisify(fs.readFile)('index.user.js', 'utf-8');
+                    js = js[js.length - 1] === '\n' ? js.slice(0, -1) : (js + '\n');
+                    await util.promisify(fs.writeFile)('index.user.js', js, 'utf-8');
+                    console.log((js[js.length - 1] === '\n' ? 'Added' : 'Removed') + ' newline to trigger reload');
+                }, 250);
+            });
         });
 	});
 
--- index.user.js
+++ index.user.js
@@ -20,51 +20,55 @@ const THROTTLE_REQUESTS = 2000;
 const BBN = {
     GM: GM,
     domain: unsafeWindow.location.host,
+    term: null,
+    courses: null,
+    updated: null,
     log: (...data) => {
         GM.log('[BBN]', ...data);
     },
     start: async () => {
         BBN.log('Starting BetterBetterNavigate...');
-
+        
         BBN.log('Waiting for DOM...');
-        await BBN.exists('app-header nav hi-link');
+        await BBN.waitForElements('app-header nav hi-link');
 
-        BBN.log('Retrieving data...');
+        BBN.log('Loading data...');
         const terms = JSON.parse(await BBN.fetch(`https://${BBN.domain}/api/v1/cat/terms/`));
         const now = Date.now();
-        BBN.term = terms.data
+        await GM.setValue('term', BBN.term = terms.data
             .filter(t => !isNaN(new Date(t.term_start_dt).getTime()))
             .sort((a, b) =>
                 Math.abs(new Date(a.term_start_dt).getTime() - now) - Math.abs(new Date(b.term_start_dt).getTime() - now)
-            )[0];
+            )[0]);
+        BBN.courses = await GM.getValue('courses', []);
+        BBN.updated = await GM.getValue('updated', null);
 
         BBN.log('Adding styles...')
-        await BBN.create(document.head, '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">');
-        await BBN.create(document.head, '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css">');
-        await BBN.create(document.head, '<script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>');
+        await BBN.injectElement(document.head, '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">');
+        await BBN.injectElement(document.head, '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css">');
+        await BBN.injectElement(document.head, '<script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>');
         document.querySelectorAll('.container').forEach(c => c.classList.remove('container'));
 
         BBN.log('Adding course browser...');
         const overlay = document.createElement('div');
+        overlay.id = 'course-browser';
         overlay.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100vh; overflow: auto; background: #fff; z-index: 99999; display: none';
         overlay.innerHTML = GM.getResourceText('course-browser');
         document.body.appendChild(overlay);
-        await BBN.runScripts(overlay);
-
+        await BBN.executeScripts(overlay);
         
         BBN.log('Adding navigation...');
         const temp = document.createElement('div');
         temp.innerHTML = `<a aesthetic="navigation" class="mlsxxx ng-scope" aria-current="false" style="padding-right: var(--hi-size--small-xxx); text-decoration: none"><span class="text-sz-l">Browse Courses</span></hi-link>`;
         const browseTab = temp.firstChild;
         browseTab.addEventListener('click', () => overlay.style.display = 'block');
-        BBN.closeBrowser = () => overlay.style.display = 'none';
         const scheduleTab = document.querySelector('app-header nav hi-link:last-of-type');
         scheduleTab.insertAdjacentElement('beforebegin', browseTab);
         temp.remove();
         
         BBN.log('Started!');
     },
-    exists: (querySelector) => {
+    waitForElements: (querySelector) => {
         return new Promise((resolve) => {
             const elements = document.querySelectorAll(querySelector);
             if (elements.length) {
@@ -81,15 +85,15 @@ const BBN = {
             observer.observe(document.body, { childList: true, subtree: true });
         });
     },
-    create: async (parent, html) => {
+    injectElement: async (parent, html) => {
         const temp = document.createElement('div');
         temp.innerHTML = html;
         const element = temp.firstChild;
         parent.appendChild(element);
-        await BBN.runScripts(element);
+        await BBN.executeScripts(element);
         return element;
     },
-    runScripts: async (element) => {
+    executeScripts: async (element) => {
         const runScript = async (oldScript) => {
             const newScript = document.createElement('script');
             let source = oldScript.innerHTML;
@@ -101,45 +105,23 @@ const BBN = {
         const childScripts = element.querySelectorAll('script');
         for (let i = 0; i < childScripts.length; i++) await runScript(childScripts[i]);
     },
-    refreshCourses: async () => {
-        const button = document.getElementById('refresh-courses');
-        const status = document.getElementById('courses-status');
-        button.setAttribute('disabled', true);
-
-        status.innerText = 'downloading courses...';
+    refreshCourses: async (status=()=>{}) => {
+        status({ step: 'downloading courses', current: 0, total: 1 });
         const courses = JSON.parse(await BBN.fetch(`https://${BBN.domain}/api/v1/cat/course_search/?term=${BBN.term.id}&only_available=false&offset=0&limit=1000`));
-        
-        status.innerText = 'downloading professors...';
-        let current = 1;
+
+        status({ step: 'downloading sections', current: 0, total: courses.data.length });
+        let current = 0;
         for (const course of courses.data) {
-            status.innerText = `downloading professors... ${current++}/${courses.data.length}`;
-            course.professors = [];
+            status({ step: 'downloading sections', current: ++current, total: courses.data.length });
             const response = await BBN.fetch(`https://${BBN.domain}/api/v1/cat/sections/term/${BBN.term.id}/course/${course.id}/section_type/0/`);
             const sections = JSON.parse(response).section;
-            for (const section of Object.values(sections)) {
-                if (!course.professors.includes(section.instructor_name))
-                    course.professors.push(section.instructor_name);
-            }
+            course.sections = Object.values(sections);
             await new Promise(resolve => setTimeout(resolve, THROTTLE_REQUESTS));
         }
-        status.innerText = 'finishing up...';
-        GM.setValue('courses', courses.data);
-        GM.setValue('courses-refreshed', new Date().toISOString());
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        unsafeWindow.location.reload();
-    },
-    refreshedCoursesAt: async () => {
-        const then = new Date(await GM.getValue('courses-refreshed'));
-        const now = new Date();
-        const diff = Math.floor((now - then) / 1000);
-        let timeAgo = '';
-        if (diff < 60) timeAgo = `${diff} seconds ago`;
-        else if (diff < 3600) timeAgo = `${Math.floor(diff / 60)} minutes ago`;
-        else if (diff < 86400) timeAgo = `${Math.floor(diff / 3600)} hours ago`;
-        else if (diff < 2592000) timeAgo = `${Math.floor(diff / 86400)} days ago`;
-        else if (diff < 31536000) timeAgo = `${Math.floor(diff / 2592000)} months ago`;
-        else timeAgo = `${Math.floor(diff / 31536000)} years ago`;
-        return `as of ${timeAgo}`;
+
+        status({ step: 'finishing up', current: 0, total: 1 });
+        await GM.setValue('courses', BBN.courses = courses.data);
+        await GM.setValue('updated', BBN.updated = new Date().toISOString());
     },
     fetch: async (url) => {
         return await new Promise((resolve) => {
